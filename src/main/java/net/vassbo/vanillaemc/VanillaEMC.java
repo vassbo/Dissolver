03ff3253bb49f3766a5d5fcc48f2f09c4799d518
Kristoffer Vassbø <kristoffervassbo@gmail.com>
2024-07-19 21:10:10 +0200

    Initial commit

---
+++ src/main/java/net/vassbo/vanillaemc/VanillaEMC.java
@@ -0,0 +1,20 @@
+package net.vassbo.vanillaemc;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class VanillaEMC implements ModInitializer {
+	public static final String MOD_ID = "vanillaemc";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
