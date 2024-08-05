09fca52801f2ca11fcbb555e57da58b9218b500c
Kristoffer Vassbø <kristoffervassbo@gmail.com>
2024-08-05 20:41:12 +0200

    📄 Description

--- src/main/java/net/vassbo/vanillaemc/config/ModConfig.java
+++ src/main/java/net/vassbo/vanillaemc/config/ModConfig.java
@@ -25,7 +25,8 @@ public class ModConfig {
 
     private static void createConfigs() {
         configs.addKeyValuePair(new Pair<>("emc_on_hud", false), "Display current EMC on HUD (top left corner)");
-        configs.addKeyValuePair(new Pair<>("private_emc", false), "Should each player have their own EMC storage? (Note: Turning this on will disable redstone integration.)");
+        // WIP not added (Note: Turning this on will disable redstone integration.)
+        configs.addKeyValuePair(new Pair<>("private_emc", false), "Should each player have their own EMC storage?");
         configs.addKeyValuePair(new Pair<>("creative_items", false), "Should creative items have EMC?");
         configs.addKeyValuePair(new Pair<>("difficulty", "hard"), "easy | normal | hard - Changes crafting recipe for Dissolver block.");
         configs.addKeyValuePair(new Pair<>("mode", "default"), "default | skyblock - Changes some EMC values.");
