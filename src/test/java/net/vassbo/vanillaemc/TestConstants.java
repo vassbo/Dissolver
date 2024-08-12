f9992544062affd8ceea7241491f735643d0cf01
Alice <alice@little.watch>
2024-08-11 22:29:25 -0400

    prepare configs

--- src/test/java/net/vassbo/vanillaemc/TestConstants.java
+++ src/test/java/net/vassbo/vanillaemc/TestConstants.java
@@ -2,8 +2,7 @@ package net.vassbo.vanillaemc;
 
 public class TestConstants {
     public enum Modes {
-        DEFAULT("default"),
-        MOCK_SKYBLOCK("MockskyblockMode");
+        DEFAULT("default"), MOCK_SKYBLOCK("MockskyblockMode");
 
         final String value;
 
@@ -14,5 +13,31 @@ public class TestConstants {
         public String getValue() {
             return value;
         }
+    }
+
+    public class Configs {
+        public final static String FILE_NAME = "vanillaemc.properties";
+
+
+        public enum Directories {
+            EMPTY("src/test/resources/config/empty"),
+
+            /** default file */
+            DEFAULT("src/test/resources/config/defaults"),
+
+            /** all the booleans are flipped */
+            INVERTED("src/test/resources/config/inverted");
+
+            final String value;
+
+            Directories(String value) {
+                this.value = value;
+            }
+
+            public String getValue() {
+                return value;
+            }
+        }
+
     }
 }
